50309819042118cf3f60518194968750354df860
Ashish kumar <51824533+ashishk8826@users.noreply.github.com>
2020-05-14 14:15:05 +0530

    footer adjust

--- worldlive.php
+++ worldlive.php
@@ -169,10 +169,10 @@ function showSlides() {
 
 <!-////////////////////////fotter/////////-->
 <footer class=" mb-0 ">
-  <div class="footer_style text-white text-center container-fluid py-3 ">
-    <p> Copyright By ASHISH KUMAR
+  <div class="footer_style text-white text-center container-fluid py-4 " width>
+    <p> © Copyright 2020 the ❤️ Activitizila
        </p>
-      <p>Email <a href="https://www.gmail.com/ashishkumarjashwal@gmail.com">👉ashishkumarjashwal@gmail.com</a></p>
+      
   </div>
 
   
